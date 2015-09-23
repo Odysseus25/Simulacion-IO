d4a69a6bea0ea829bb3d8bf0cd1a82511e11e6aa
Ulises Gonzalez <ulisesgz@outlook.com>
2015-09-23 15:56:20 -0600

    Tiempos de llegada de archivos

--- Simulación IO/src/simulación/io/ControladorEventos.java
+++ Simulación IO/src/simulación/io/ControladorEventos.java
@@ -46,33 +46,53 @@ public class ControladorEventos {
     
     //inicializa los eventos en tiempo cero. 
     //Crea los eventos de las maquinas, para decidiar cual maq comienza a enviar arhivo.
-    public void setTiemposEventos(){ //define los tiempos iniciales
+    public void setEventTime(){ //define los tiempos iniciales
         //se crean los eventos
         Event maqA = new Event();
         Event maqB = new Event();
-        //Event maqC = new Event();
+        Event maqC = new Event();
         
         //se llenan los atributos de los eventos
         maqA.numEvent = 1;
-        maqA.time = (-1*(Math.log(1- Math.random())))/5;
+        maqA.time = timeFileA();
         maqA.nombre= "Llega archivo a maquina A";
         
         maqB.numEvent = 2;
-        maqB.time = randomWithRange(8, 12)/40;
+        maqB.time = timeFileB();
         maqB.nombre = "Llega archivo a B";
        
-        //averiguar como sacar tiempo de dist normal
-        /*maqC.numEvent = 2;
-        maqC.time = ;
-        maqC.nombre = "Llega archivo a C"; */ 
+        maqC.numEvent = 3;
+        maqC.time = timeFileC();
+        maqC.nombre = "Llega archivo a C";
         
         //se agregan eventos a la cola
         events.add(maqA);
         events.add(maqB);
-        //events.add(timeC);
+        events.add(maqC);
     }
     
-    //falta la logica para programar los eventos
+    //tiempos de llegadas de archivos a las maquinas
+    public double timeFileA(){
+        double temp = Math.random();
+        while(temp == 1){
+            temp = Math.random();
+        }
+        return (-1*(Math.log(1-temp)))/5;
+    }
+    
+    public double timeFileB(){
+        return randomWithRange(8, 12)/40;
+    }  
+    
+    public double timeFileC(){
+        double addedValues = 0;
+        for(int i = 0; i <= 12; i++){
+            addedValues += Math.random();
+        }
+        return 0.1*(addedValues-6)+5;
+    } 
+    
+    
     public void llegaArchivoA(){
         clock = (events.poll()).getTime();
     }
