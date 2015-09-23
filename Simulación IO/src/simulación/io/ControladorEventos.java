adc87a3aeb4cdabf5fc9b3d9d02c15725021e855
Ulises Gonzalez <ulisesgz@outlook.com>
2015-09-23 17:52:11 -0600

    Inicializacion lista

--- Simulación IO/src/simulación/io/ControladorEventos.java
+++ Simulación IO/src/simulación/io/ControladorEventos.java
@@ -16,7 +16,7 @@ public class ControladorEventos {
     
     //clase evento de la que se crean los eventos
     static class Event{
-        int numEvent;   //id del evento
+        //int numEvent;   //id del evento
         double time;
         String nombre;
         
@@ -32,29 +32,6 @@ public class ControladorEventos {
         int tamanio;
     }
     
-    
-    /** Declaracion Variables globales **/
-    
-    //reloj global de la simulacion
-    double clock; 
-    MyComparator comparator = new MyComparator();  
-    PriorityQueue<Event> events = new PriorityQueue<>(13, comparator);  //cola de prioridad que ordena los eventos por tiempo
-    
-    ArrayList <Archive> priorityFileA1 = new ArrayList <Archive>();     // Cola de prioridad de archivos 1 maquina A
-    ArrayList <Archive> priorityFileA2 = new ArrayList <Archive>();     // Cola de prioridad de archivos 2 maquina A
-        
-    ArrayList <Archive> priorityFileB1 = new ArrayList <Archive>();     // Cola de prioridad de archivos 1 maquina B
-    ArrayList <Archive> priorityFileB2 = new ArrayList <Archive>();     // Cola de prioridad de archivos 2 maquina B
-    
-    ArrayList <Archive> priorityFileC1 = new ArrayList <Archive>();     // Cola de prioridad de archivos 1 maquina C
-    ArrayList <Archive> priorityFileC2 = new ArrayList <Archive>();     // Cola de prioridad de archivos 2 maquina C
-    
-    
-    
-    // ---------------- Fin de declaracion---------------------------------------- //
-    
-    
-    
     //clase que compara el tiempo de reloj de los eventos para ordenarlos
     //Es usada por el priority queue para ordenar los eventos
     static class MyComparator implements Comparator<Event> {
@@ -64,6 +41,25 @@ public class ControladorEventos {
         return result;
     }
 }
+    /** Declaracion Variables globales **/
+    
+    //reloj global de la simulacion
+    double clock; 
+    MyComparator comparator = new MyComparator();  
+    PriorityQueue<Event> events = new PriorityQueue<>(13, comparator);  //cola de prioridad que ordena los eventos por tiempo
+    
+    ArrayList <File> priorityFileA1 = new ArrayList <File>();     // Cola de prioridad de archivos 1 maquina A
+    ArrayList <File> priorityFileA2 = new ArrayList <File>();     // Cola de prioridad de archivos 2 maquina A
+        
+    ArrayList <File> priorityFileB1 = new ArrayList <File>();     // Cola de prioridad de archivos 1 maquina B
+    ArrayList <File> priorityFileB2 = new ArrayList <File>();     // Cola de prioridad de archivos 2 maquina B
+    
+    ArrayList <File> priorityFileC1 = new ArrayList <File>();     // Cola de prioridad de archivos 1 maquina C
+    ArrayList <File> priorityFileC2 = new ArrayList <File>();     // Cola de prioridad de archivos 2 maquina C
+    
+    
+    
+    // ---------------- Fin de declaracion---------------------------------------- //
     
     //genera randoms en un intervalo
     public int randomWithRange(int min, int max){
@@ -74,28 +70,58 @@ public class ControladorEventos {
     //inicializa los eventos en tiempo cero. 
     //Crea los eventos de las maquinas, para decidiar cual maq comienza a enviar arhivo.
     public void setEventTime(){ //define los tiempos iniciales
+        double temp;
         //se crean los eventos
         Event maqA = new Event();
         Event maqB = new Event();
         Event maqC = new Event();
         
         //se llenan los atributos de los eventos
-        maqA.numEvent = 1;
+        //maqA.numEvent = 1;
         maqA.time = timeFileA();
         maqA.nombre= "Llega archivo a maquina A";
+        temp =maqA.getTime();
         
-        maqB.numEvent = 2;
+        //maqB.numEvent = 2;
         maqB.time = timeFileB();
         maqB.nombre = "Llega archivo a B";
-       
-        maqC.numEvent = 3;
+        if(temp > maqB.getTime()){
+            temp = maqB.getTime();
+        }
+        
+        //maqC.numEvent = 3;
         maqC.time = timeFileC();
         maqC.nombre = "Llega archivo a C";
+        if(temp > maqC.getTime()){
+            temp = maqC.getTime();
+        }
         
         //se agregan eventos a la cola
         events.add(maqA);
-        events.add(maqB);
+        events.add(maqB );
         events.add(maqC);
+        
+        double rand = Math.random()*100;
+        if(rand > 0 && rand < 32){
+            Event rcvTkA = new Event();
+            rcvTkA.nombre = "Recibe token A";
+            rcvTkA.time = temp + 3; 
+            events.add(rcvTkA);
+        }
+        else{
+            if(rand > 33 && rand < 65){
+                Event rcvTkB = new Event();
+                rcvTkB.nombre = "Recibe token B";
+                rcvTkB.time = temp + 3; 
+                events.add(rcvTkB);
+            }
+            else{
+                Event rcvTkC = new Event();
+                rcvTkC.nombre = "Recibe token C";
+                rcvTkC.time = temp + 3; 
+                events.add(rcvTkC);
+            }
+        }
     }
     
     //tiempos de llegadas de archivos a las maquinas
@@ -119,6 +145,19 @@ public class ControladorEventos {
         return 0.1*(addedValues-6)+5;
     } 
     
+    //define la prioridad del archivo
+    public int filePriority(){
+        int temp = Math.random()*4;
+        if(temp == 0){
+            return 1;
+        }
+        return 2;
+    }
+    
+    //define el tamano de cada archivo
+    public int fileSize(){
+     return randomWithRange(1, 64);   
+    }
     
     public void llegaArchivoA(){
         //clock = (events.poll()).getTime();
@@ -137,7 +176,8 @@ public class ControladorEventos {
     public void terminaEnviarArchivo(){
     }
     
-    public void recibeTokenA(){
+    public void ReceivesTokenA(){
+        clock = 
     }
     
     public void recibeTokenB(){
